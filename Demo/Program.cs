cededc86d3f492efd9ed323e8be6f74f881e1c83
Denis Danevski <danevskidenis@gmail.com>
2024-06-20 15:01:37 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // fib(0) = 1
+            // fib(1) = 1
+            // fib(n) = fib(n-1) + fib(n-2)
+            // (0) 1 1 2 3 5 8 13 21 34 ...
+
+            DateTime start = DateTime.Now;
+
+            long eighthFibNumber = FibonacciIterative(50);
+            Console.WriteLine(eighthFibNumber);
+
+            DateTime finish = DateTime.Now;
+            TimeSpan diff = finish - start;
+            Console.WriteLine($"Elapsed time: {diff.TotalSeconds:f5}");
+        }
+
+        // n = 40 => 1.01526s
+        // n = 50 => 95.34920s
+        static long FibonacciRecursive(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+
+        // n = 40 => 0.01126s
+        // n = 50 => 0.01058s
+        static long FibonacciIterative(int n)
+        {
+            long a = 1; long b = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                long next = a + b;
+                a = b;
+                b = next;
+            }
+
+            return b;
+        }
+
+        static void Demo(int n) // max(n^2, 1) => O(n^2)
+        {
+            if(n % 2 == 0) // O(n^2)
+            {
+                for (int i = 0; i < n; i++) // n iterations, in total n*n=n^2
+                {
+                    for(int j = 0; j < n; j++) // n iterations
+                    {
+                        Console.WriteLine($"{i}, {j}"); 
+                    }
+                }
+            }
+            else // O(1)
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
---
+++ Lists/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* // array that will be growing in time
+
+             CustomList list = new CustomList();
+             list.Add(1);
+             list.Add(13);
+             list.Add(7);
+             list.Add(7);
+             list.Add(7);
+             list.Add(7);
+             list.Add(7);
+
+             Console.WriteLine(list[2]);
+
+             list[5] = 100;
+             Console.WriteLine(list[5]);
+
+             Console.WriteLine($"Element at index 6 before insert: {list[6]}");
+             list.Insert(6, 200);
+             Console.WriteLine($"Element at index 6 after insert: {list[6]}");
+             list.Insert(8, 300);
+
+             Console.WriteLine($"List count before remove: {list.Count}");
+             list.RemoveAt(0);
+             Console.WriteLine($"List count after remove: {list.Count}");*/
+
+            CustomDoublyLinkedList list = new CustomDoublyLinkedList();
+
+            for (int i = 0; i < 10; i++) list.Add(i);
+
+            Console.WriteLine(list.Count);
+
+            for (int i = 0; i < 10; i++) list[i] += 5;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Inserting element at index {i}");
+                list.Insert(i, i * 2);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"Value at index {i}: {list[i]}");
+            }
+
+            for (int i = 0;i < 3; i++) 
+            {
+                Console.WriteLine($"Removing element at index {0}");
+                list.RemoveAt(0);
+
+                Console.WriteLine($"Removing element at index {list.Count / 2}");
+                list.RemoveAt(list.Count / 2);
+                
+                Console.WriteLine($"Removing element at index {list.Count - 1}");
+                list.RemoveAt(list.Count - 1);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"Value at index {i}: {list[i]}");
+            }
+
+            //IEnumerable
+            Console.WriteLine(string.Join(", ", list));
+
+            /* int elementsCount = list.Count;
+            for(int i = 0; i < elementsCount; i++) list.RemoveAt(0); //graphs(BFS)
+
+            // while (list.Count > 0) list.RemoveAt(0);  //queues, stacks
+
+            Console.WriteLine($"List has {list.Count} elements left.");*/
+
+            // 1. Access the value at index i
+            // 2. Insert
+            // 3. RemoveAt
+            // 4. Use in loops
+        }
+    }
+}
